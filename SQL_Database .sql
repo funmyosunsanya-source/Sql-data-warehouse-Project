fa3cdb279bd7d9255e13451f2cfd74c2266f6904
funmyosunsanya-source <funmyosunsanya@yahoo.com>
2026-03-13 06:30:41 +0100

    Create SQL_Database .sql

---
+++ SQL_Database .sql
@@ -0,0 +1,16 @@
+Use the chatflow for work purposes to create scha for this project
+
+WARNINGS:
+
+USE master;
+
+create database DataWarehouse;
+
+use datawarehouse;
+
+Create schema bronze;
+Go
+create schema silver;
+GO
+Create schema gold;
+go
